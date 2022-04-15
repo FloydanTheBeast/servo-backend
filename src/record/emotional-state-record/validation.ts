3d3ac426af0b0e9edfdabad2b669950f0249e205
FloydanTheBeast <floydanthebeast@gmail.com>
2022-04-15 13:42:32 +0300

    fix: Fix validation schema for emotional state record

--- src/record/emotional-state-record/validation.ts
+++ src/record/emotional-state-record/validation.ts
@@ -1,6 +1,7 @@
 import Joi from 'joi';
 
 export const EmotionalStateRecordSchema = Joi.object({
+  state: Joi.allow(),
   description: Joi.string().max(200),
   date: Joi.date().required().less('now'),
 });
